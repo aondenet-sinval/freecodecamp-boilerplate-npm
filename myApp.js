4d86458a0654d524bdd772f3d668764f57a48a9a
aondenet-sinval <sinvalsatv@gmail.com>
2023-05-15 10:13:55 -0300

    desafio 4

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let app = express();
 
+app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res)=>{
   const absolutePath = __dirname + '/views/index.html'
   res.sendFile(absolutePath)
