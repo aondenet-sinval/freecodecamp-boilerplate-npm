95016da7cb92baeda56c05bacc2b0aa5d6d20418
aondenet-sinval <sinvalsatv@gmail.com>
2023-05-15 21:30:23 -0300

    add pass 8

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let express = require('express');
 let app = express();
 
 app.use('/public', express.static(__dirname + '/public'))
+
+app.use((req, res, next)=>{
+  console.log(req.method, req.path, '-', req.ip);
+})
 app.get('/', (req, res)=>{
   const absolutePath = __dirname + '/views/index.html'
   res.sendFile(absolutePath)
