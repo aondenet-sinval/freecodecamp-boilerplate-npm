98dbbbe8690b4661bbe4abdff93eceb0d9f8ecaf
aondenet-sinval <sinvalsatv@gmail.com>
2023-05-15 21:34:56 -0300

    add pass 8 next()

--- myApp.js
+++ myApp.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 let express = require('express');
 let app = express();
 
-app.use('/public', express.static(__dirname + '/public'))
-
 app.use((req, res, next)=>{
   console.log(req.method, req.path, '-', req.ip);
   next()
 })
+
+app.use('/public', express.static(__dirname + '/public'))
+
 app.get('/', (req, res)=>{
   const absolutePath = __dirname + '/views/index.html'
   res.sendFile(absolutePath)
