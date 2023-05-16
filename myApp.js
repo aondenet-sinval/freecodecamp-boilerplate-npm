ced0aea0b38cd4f12569ae250c977bb713f1153e
aondenet-sinval <sinvalsatv@gmail.com>
2023-05-15 21:48:12 -0300

    add pass 9

--- myApp.js
+++ myApp.js
@@ -23,6 +23,15 @@ app.get('/json', (req, res)=>{
 })
 
 
+app.get('/now', (req, res, next)=>{
+  req.time = new Date().toString()
+  next()
+}, (req, res)=>{
+  res.json({'time': req.time})
+}
+)
+
+
 
 
 
